74373323d467e12dd593cf4b609dd93c10bcdf82
Mohammed <baroud1997@hotmail.com>
2024-05-09 22:46:12 +0300

    create service page

--- homeCareApp/views.py
+++ homeCareApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render , redirect
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def service(request):
+    return render(request,'services.html')
