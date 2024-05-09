91d2ca5a4fe0026633cb6921a9f159bbe2e3f967
AnwarKhanfa <Khanfaanwar@gmail.com>
2024-05-09 15:44:35 +0300

    test

--- homeCareApp/views.py
+++ homeCareApp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render , redirect
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def aboutus(request):
+    return render(request,'aboutus.html')
+
+def coverpage(request):
+    return render(request,'coverpage.html')
